992df26f3451e82d6214593dd3b6f7454dcc1473
danielherrera1982 <55557526+danielherrera1982@users.noreply.github.com>
2024-12-18 04:39:50 +0000

    commit all

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as ptl
 import seaborn as sns
 import requests
+import matplotlib.pyplot as plt
 
 # URL del web service
 url = "https://api.latam-ia.com/api/Measures" 
@@ -34,3 +35,21 @@ st.dataframe(data, hide_index=True, use_container_width=True)
 #Histograma
 histograma = ptl.histogram(data, x='measure', title='Histograma de edades', nbins=10)
 st.plotly_chart(histograma)
+
+# grafico
+
+filtered_df = data[data['sensor_uq'] == 'A228DE51-023B-43B0-B652']
+# Asegurarse de que la columna add_date sea de tipo datetime
+#filtered_df['add_date'] = pd.to_datetime(filtered_df['add_date'], errors='coerce')
+# Configurar el índice del DataFrame para usar add_date
+filtered_df = filtered_df.sort_values('add_date')  # Ordenar por fecha
+#filtered_df.set_index('add_date', inplace=True)
+
+st.write(f"Análisis con data filtrada")
+st.dataframe(filtered_df, hide_index=True, use_container_width=True)
+
+ciudad_desercion = ptl.line(filtered_df ,x='add_date', y='measure', title='Deserción por ciudad')
+st.plotly_chart(ciudad_desercion)
+
+#plot = plt.plot(filtered_df.index, filtered_df['measure'], label='Temperatura', marker='o', linestyle='-', color='blue')
+#st.plotly_chart(plot)
