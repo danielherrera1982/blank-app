989b4a054fc8c2da71d0473a0047af9ac054c5a0
danielherrera1982 <55557526+danielherrera1982@users.noreply.github.com>
2024-12-18 04:16:33 +0000

    commit all

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as ptl
+import seaborn as sns
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# URL del web service
+url = "https://api.latam-ia.com/api/Measures" 
+
+@st.cache_data
+def load_sensors(rows=500):
+  try:
+    # Hacer la solicitud GET
+    response = requests.get(url)
+    
+    # Comprobar si la solicitud fue exitosa
+    if response.status_code == 200:
+        # Convertir la respuesta JSON a un DataFrame
+        json = response.json()
+        data = pd.DataFrame(json)
+    return data
+  except Exception as e:
+    print(f"Ocurrió un error: {e}")
+
+data = load_sensors()
+
+st.title("Dashboard IoT")
+st.sidebar.title("Filtros")
+mostrar = st.sidebar.checkbox("Mostrar análisis completo")
+
+st.write(f"Análisis con data completa")
+st.dataframe(data, hide_index=True, use_container_width=True)
+
+#Histograma
+histograma = ptl.histogram(data, x='measure', title='Histograma de edades', nbins=10)
+st.plotly_chart(histograma)
